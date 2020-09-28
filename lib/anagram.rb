d584b260872197c59b2c9b2caf90405a5064418a
DavidRMorphew <morphewd@umich.edu>
2020-09-28 15:38:27 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        self.word = word
+    end
+
+    def word_split_alphabetized(any_word)
+        any_word.split(//).sort
+    end
+
+    def match(anagrams)
+        @anagrams = anagrams
+        @anagrams.select {|anagram| word_split_alphabetized(anagram) == word_split_alphabetized(self.word)}
+       # binding.pry
+    end
+
+end
