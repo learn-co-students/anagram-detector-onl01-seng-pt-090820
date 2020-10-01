bfbdc887841f79e7e9ff3b0d5c1efdd5b6518bc2
Christian Simon <csimon1127@gmail.com>
2020-10-01 18:26:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(words)
+    ret_arr = []
+    counter = 0
+    words.each { |anagram|
+      if anagram.length == word.length 
+        temp_word = anagram.split(//).sort
+        compare = @word.split(//).sort
+        if compare == temp_word
+          ret_arr << anagram
+        end
+      end
+      counter = 0
+      
+      
+    }
+    ret_arr
+  end 
+  
+  
+end 
