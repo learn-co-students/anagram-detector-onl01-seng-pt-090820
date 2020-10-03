7a7ac54e4576391ef8858332d6d280a836c82d75
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-10-03 19:11:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.select { |string| (string.split("").sort) == (@word.split("").sort) }
+  end
+  
+end
